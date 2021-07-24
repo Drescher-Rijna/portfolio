f6706b35864b9ef2036e92ba7f71cbc12fbfb0d4
Drescher-Rijna <82637513+Drescher-Rijna@users.noreply.github.com>
2021-07-24 22:27:47 +0200

    initial setup done

--- components/NavbarDesktop.js
+++ components/NavbarDesktop.js
@@ -1,9 +1,33 @@
 const NavbarDesktop = () => {
     return (
         <nav className="global-menu-desktop">
+            <div>
+                LOGO
+            </div>
             <ul className="menu-liste-desktop">
-
+                <li>
+                    Om mig
+                </li>
+                <li>
+                    Projekter
+                </li>
+                <li>
+                    Skillset
+                </li>
+                <li>
+                    Kontakt mig
+                </li>
             </ul>
+            <div className="socials-desktop">
+                <ul>
+                    <li>
+                        GH
+                    </li>
+                    <li>
+                        LI
+                    </li>
+                </ul>
+            </div>
         </nav>
     )
 }
