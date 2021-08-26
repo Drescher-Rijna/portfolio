32f185d8b5c5bf99054e61120b9c4de1af996ec6
Drescher-Rijna <82637513+Drescher-Rijna@users.noreply.github.com>
2021-08-26 21:06:52 +0200

    done

---
+++ pages/tak-for-beskeden/index.js
@@ -0,0 +1,32 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faCheck } from '@fortawesome/free-solid-svg-icons';
+import Link from 'next/link';
+
+const TakForBeskeden = () => {
+
+    return(
+        <div className="h-full">
+            <div className="bg-green-600 pl-4 py-1" >
+                <Link href='/'>
+                    <img src="/logo-black-white.png" className="w-14 cursor-pointer" />
+                </Link>
+            </div>
+            <div className="grid place-content-center place-items-center gap-y-12 my-20 mx-4 md:my-40 md:mx-20 lg:mx-48 lg:my-32 xl:mx-72 xl:my-40">
+                <h2 className="heading-h2 text-center">
+                    Tak for din besked
+                </h2>
+                
+                <FontAwesomeIcon icon={faCheck} className="text-green-600 text-9xl text-center" />
+                
+                <Link href='/'>
+                    <h3 className="heading-h3 cursor-pointer text-center hover:text-green-600 hover:underline">
+                        Du kan komme tilbage til kontaktformen ved at klikke her
+                    </h3>
+                </Link>
+            </div>
+        </div>
+                
+    );
+}
+
+export default TakForBeskeden;
