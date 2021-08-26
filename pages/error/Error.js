afacfc7ca204118eccf1217f19b2f3fabb213e2e
Drescher-Rijna <82637513+Drescher-Rijna@users.noreply.github.com>
2021-08-26 20:26:38 +0200

    fixed

---
+++ pages/error/Error.js
@@ -0,0 +1,13 @@
+const ErrorMessage = () => {
+
+    return(
+        <div>
+            Der skete en fejl under forsendelsen. Være sikker på at alle felter er udfyldt korrekt.
+            Font awesome x ikon
+            Du kan komme tilbage til kontaktformen ved at klikke her
+        </div>
+                
+    );
+}
+
+export default ErrorMessage;
---
+++ pages/tak-for-beskeden/TakForBeskeden.js
@@ -0,0 +1,14 @@
+const TakForBeskeden = () => {
+
+    return(
+        <div>
+            Tak for din besked
+            FontAwesome check
+            Jeg vil kigge på og svare på jeres besked hurtigst muligt. Glæder mig til at samarbejde med jer.
+            Du kan komme tilbage til siden ved at klikke her
+        </div>
+                
+    );
+}
+
+export default TakForBeskeden;
